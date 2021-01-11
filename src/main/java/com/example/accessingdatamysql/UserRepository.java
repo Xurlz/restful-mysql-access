313fa817ee4aa6199ce0ab7ba7c59fa7f26dbd80
Xurlz <charlessena2010@bol.com.br>
2021-01-11 17:17:40 -0300

    Inclusão de projeto

---
+++ src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.accessingdatamysql.User;
+
+public interface UserRepository extends CrudRepository<User, Integer>{}
